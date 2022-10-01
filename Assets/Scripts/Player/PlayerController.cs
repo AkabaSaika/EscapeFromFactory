80eb49a9b26c8da6d509a5863fb45f11c9906163
AkabaSaika <akabataiho@gmail.com>
2022-10-02 02:49:26 +0900

    增加了摄像机锁定功能

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,309 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using Bright.Serialization;
+using cfg;
+
+public class PlayerParameter
+{
+    private float maxHp;
+    private float maxStamina;
+    //private float defendPoint = 10.0f;
+    private float walkSpeed;
+    private float runSpeed;
+    private float jumpSpeed;
+    
+
+    public float MaxHp { get => maxHp; set => maxHp = value; }
+    public float MaxStamina { get => maxStamina; set => maxStamina = value; }
+    //public float DefendPoint { get => defendPoint; set => defendPoint = value; }
+    public float WalkSpeed { get => walkSpeed; set => walkSpeed = value; }
+    public float RunSpeed { get => runSpeed; set => runSpeed = value; }
+    public float JumpSpeed { get => jumpSpeed; set => jumpSpeed = value; }
+}
+
+public class PlayerController : MonoBehaviour
+{
+    private PlayerParameter parameter = new PlayerParameter();
+    private cfg.character.Character playerParameterData;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float currentHp;
+    [SerializeField]
+    private float jumpSpeed;
+    private float mouseSensiticity;
+    private float angleY;
+    private float angleX;
+    [SerializeField]
+    private bool groundState = false;
+    private bool isDead = false;
+    private Vector3 camFoward;
+
+    private float h;
+    private float v;
+
+    private bool isMoving;
+
+    private CharacterController cc;
+    private Transform mainCamera;
+    private CameraController cameraController;
+
+    private CollisionFlags cf;
+ 
+    [SerializeField]
+    private bool isGrounded;
+    private bool canCancel = false;
+
+    private int speedZID = Animator.StringToHash("SpeedZ");
+    private int speedRotateID = Animator.StringToHash("SpeedRotate");
+    private int speedHID = Animator.StringToHash("SpeedH");
+    private int speedVID = Animator.StringToHash("SpeedV");
+    private int hitId = Animator.StringToHash("Hit");
+    private int deadId = Animator.StringToHash("IsDead");
+    private int JumpId = Animator.StringToHash("Jump");
+    private int LandId = Animator.StringToHash("Land");
+    
+    private Animator anim;
+    private WarriorAnimsFREE.IKHands IKHands;
+
+    public float CurrentHp { get => currentHp; set => currentHp = value; }
+
+    private void Awake()
+    {
+        playerParameterData = TablesSingLeton.Instance.Tables.TbCharacter.Get(1001);
+        parameter.MaxHp = playerParameterData.MaxHp;
+        parameter.WalkSpeed = playerParameterData.WalkSpeed;
+        parameter.RunSpeed = playerParameterData.Runspeed;
+        parameter.JumpSpeed = playerParameterData.JumpSpeed;
+        CurrentHp = parameter.MaxHp;
+    }
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        cc = GetComponent<CharacterController>();
+        IKHands = GetComponent<WarriorAnimsFREE.IKHands>();
+
+        speed = parameter.WalkSpeed;
+        mouseSensiticity = 2.4f;
+        angleY = transform.eulerAngles.y;
+        mainCamera = Camera.main.transform;
+        cameraController = mainCamera.GetComponent<CameraController>();
+        angleX = mainCamera.eulerAngles.x;
+        Cursor.visible = false;
+        //Cursor.lockState = CursorLockMode.Locked;
+        isGrounded = groundState;   
+    }
+
+    private void FixedUpdate()
+    {
+        if(!isDead)
+        {
+            h = Input.GetAxis("Horizontal");
+            v = Input.GetAxis("Vertical");
+            //Jump();
+            isMoving = Mathf.Abs(h) > 0 || Mathf.Abs(v) > 0;
+        }
+    }
+
+    private void Update()
+    {
+        if(!isDead)
+        {
+            camFoward = Vector3.ProjectOnPlane(mainCamera.forward, Vector3.up);
+            
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                cameraController.LockOn();
+            }
+            if (cameraController.IsLockOn)
+            {
+                MoveWhileLock();
+
+            }
+            else
+            {
+                Move();
+            }
+
+
+            //特定のアニメションを再生する時にIKの適用状態を調整する
+            if (anim.GetCurrentAnimatorClipInfo(0)[0].clip.name.Equals("great sword idle (5)"))
+            {
+                IKHands.SetIKOff();
+            }
+            else
+            {
+                IKHands.SetIKOn();
+            }
+
+            //ゲームクリアの判定処理
+            RaycastHit hit;
+            if(Physics.Linecast(gameObject.transform.position,gameObject.transform.position+new Vector3(0,-0.5f,0),out hit,1<<10))
+            {
+                if(hit.collider.gameObject.name=="GameClear")
+                {
+                    //GameObject.Find("Game").GetComponent<GameManager>().clear();
+                    GameManager.Instance.gameClearHandler.Invoke();
+                }
+            }
+        }
+        
+
+        
+    }
+
+    private void LateUpdate()
+    {
+        if (!isDead && !cameraController.IsLockOn)
+        {
+            Turn();
+        }
+    }
+
+    private void Move()
+    {
+        Vector3 lookAtPoint = new Vector3(h, 0, v);
+        speed = Input.GetButton("Run") ? parameter.RunSpeed : parameter.WalkSpeed;
+        //transform.Translate(camFoward * v * speed * Time.deltaTime,Space.World);
+        //transform.Translate(mainCamera.right * h * speed * Time.deltaTime,Space.World);
+        
+        anim.SetFloat(speedVID, Convert.ToSingle(isMoving) * speed);
+    }
+
+    private void MoveWhileLock()
+    {
+        speed = Input.GetButton("Run") ? parameter.RunSpeed : parameter.WalkSpeed;
+        anim.SetFloat(speedHID, h * speed);
+        anim.SetFloat(speedVID, v * speed);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButton("Jump") && isGrounded)
+        {
+            Debug.Log(Input.GetButton("Jump"));
+            jumpSpeed = parameter.JumpSpeed;
+            isGrounded = false;
+            anim.SetTrigger(JumpId);
+        }
+        if (!isGrounded)
+        {
+            jumpSpeed -= 10 * Time.deltaTime;
+            Vector3 jump = new Vector3(0, jumpSpeed * Time.deltaTime, 0);
+            cf = cc.Move(jump);
+            if (cf == CollisionFlags.Below)
+            {
+                anim.SetTrigger(LandId);
+                jumpSpeed = 0;
+                isGrounded = true;   
+            }
+        }
+        if (isGrounded && cf == CollisionFlags.None)
+        {
+            isGrounded = false;
+        }
+    }
+
+    public void Damaged(HitEvent hitEvent)
+    {
+        anim.SetTrigger(hitId);
+
+        if(CurrentHp<=0)
+        {
+            AudioManager.Instance.EffectPlay("Audio/Voice/univ1077",false); //死亡ボイスを再生
+
+            gameObject.GetComponent<CharacterController>().enabled = false;
+            isDead = true;
+            anim.SetTrigger(deadId);
+        }
+        else
+        {
+            //受撃ボイスを再生
+            int i = UnityEngine.Random.Range(0, 4);
+            string path = "Audio/Voice/univ" + (1091 + i).ToString();
+            AudioManager.Instance.EffectPlay(path, false);
+
+            CurrentHp -= hitEvent.Damage; //ライフポイントを更新
+        }
+
+        Debug.Log("hit by" + hitEvent.SkillName);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Door"))
+        {
+            DoorController dc = other.GetComponentInParent<DoorController>();
+            dc.doorChange += delegate { DoorController.OpenDoor(dc.door); };
+            dc.doorChange(dc.door);
+            
+        }
+        Debug.Log(other.name);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Door"))
+        {
+            DoorController dc = other.GetComponentInParent<DoorController>();
+            dc.doorChange += delegate { DoorController.CloseDoor(dc.door); };
+            dc.doorChange(dc.door);
+
+        }
+    }
+
+    private void PickUp(int id)
+    {
+        
+    }
+
+    /// <summary>
+    /// ロックオンしていない場合
+    /// </summary>
+    private void Turn()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.forward = camFoward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.forward = -camFoward;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.forward = mainCamera.right * (-1);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.forward = mainCamera.right;
+        }
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
+        {
+            transform.forward = camFoward - mainCamera.right;
+        }
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
+        {
+            transform.forward = camFoward + mainCamera.right;
+        }
+        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+        {
+            transform.forward = -camFoward - mainCamera.right;
+        }
+        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+        {
+            transform.forward = -camFoward + mainCamera.right;
+        }
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        anim.SetIKPositionWeight(AvatarIKGoal.LeftFoot,1.0f);
+        anim.SetIKPositionWeight(AvatarIKGoal.RightFoot, 1.0f);
+        anim.SetIKRotationWeight(AvatarIKGoal.LeftFoot, 1.0f);
+        anim.SetIKRotationWeight(AvatarIKGoal.RightFoot, 1.0f);
+    }
+}
---
+++ Assets/Scripts/Skills/Skill.cs
@@ -0,0 +1,341 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEditor;
+
+[Serializable]
+public class SkillParam
+{
+    [SerializeField]
+    private float m_attackPointEndTime;
+    [SerializeField]
+    private float m_backswingStartTime;
+    [SerializeField]
+    private float m_attackAnimationStartTime;
+    [SerializeField]
+    private float m_attackAnimationEndTime;
+    [SerializeField]
+    private float m_attackAnimationNormalizedStartTime;
+    [SerializeField]
+    private float m_attackAnimationNormalizedEndTime;
+    [SerializeField]
+    private int m_power;
+    [SerializeField]
+    private GameObject m_Owner;
+    [SerializeField]
+    private string m_ClipName;
+    [SerializeField]
+    private string m_EffectPath="Audio/HammerImpact4";
+    [SerializeField]
+    private string m_voicePath;
+    [SerializeField]
+    private GameObject[] hps;
+
+
+    public float AttackPointEndTime { get => m_attackPointEndTime; set => m_attackPointEndTime = value; }
+    public float BackswingStartTime { get => m_backswingStartTime; set => m_backswingStartTime = value; }
+    public float AttackAnimationStartTime { get => m_attackAnimationStartTime; set => m_attackAnimationStartTime = value; }
+    public float AttackAnimationEndTime { get => m_attackAnimationEndTime; set => m_attackAnimationEndTime = value; }
+    public string ClipName { get => m_ClipName; set => m_ClipName = value; }
+    public GameObject Owner { get => m_Owner; set => m_Owner = value; }
+    public int Power { get => m_power; set => m_power = value; }
+    public string EffectPath { get => m_EffectPath; set => m_EffectPath = value; }
+    public string VoicePath { get => m_voicePath; set => m_voicePath = value; }
+    public GameObject[] Hps { get => hps; set => hps = value; }
+    public float AttackAnimationNormalizedStartTime { get => m_attackAnimationNormalizedStartTime; set => m_attackAnimationNormalizedStartTime = value; }
+    public float AttackAnimationNormalizedEndTime { get => m_attackAnimationNormalizedEndTime; set => m_attackAnimationNormalizedEndTime = value; }
+}
+
+public class Skill : MonoBehaviour
+{
+    public SkillParam sp = new SkillParam();
+    [SerializeField]
+    private Vector3[] m_attacklinePos = new Vector3[2];
+    [SerializeField]
+    private List<GameObject> m_hitObject = new List<GameObject>();
+    public bool isAttacking;
+    private int cancelID = Animator.StringToHash("CanCancel");
+    private GameObject weapon;
+    private AnimationClip m_clip;
+    private Animator m_anim;
+    private Animation animation;
+    private AnimatorStateInfo stateInfo;
+    [SerializeField]
+    private UnityAction action;
+    private UnityAction<string> soundAction;
+
+    
+
+    public UnityAction Action { get => action; set => action = value; }
+   
+
+    private void Update()
+    {
+        OnAnimationEnd(Action);
+
+        stateInfo = m_anim.GetCurrentAnimatorStateInfo(0);
+        if(stateInfo.normalizedTime>=sp.AttackAnimationNormalizedStartTime&&stateInfo.normalizedTime<=sp.AttackAnimationNormalizedEndTime)
+        {
+            foreach (var hp in sp.Hps)
+            {
+                StartCoroutine(DrawLineFixed(hp));
+            }
+        }
+        if(stateInfo.normalizedTime>sp.AttackAnimationNormalizedEndTime)
+        {
+
+                //StopCoroutine(DrawLineFixed(hp));
+                StopAllCoroutines();
+
+        }
+   
+
+    }
+
+    /// <summary>
+    /// スキルを初期化する。
+    /// 初期化する際にこの関数が外部（武器のクラス）に呼び出される
+    /// </summary>
+    /// <param name="skillParam">パラメータを格納するオブジェクト</param>
+    /// <param name="hitPointPos">武器攻撃判定の座標</param>
+    public static SkillParam InitSkill(GameObject parent, cfg.test.SkillParam skillParam, GameObject[] hitPoints)
+    {
+        Skill skill = parent.AddComponent<Skill>() as Skill;
+        skill.sp.Owner = parent;
+        skill.m_anim = parent.GetComponent<Animator>();
+        skill.animation = parent.GetComponent<Animation>();
+        skill.sp.AttackAnimationEndTime = skillParam.AttackAnimationEndTime;
+        skill.sp.AttackAnimationStartTime = skillParam.AttackAnimationStartTime;
+        skill.sp.AttackPointEndTime = skillParam.AttackPointEndTime;
+        skill.sp.BackswingStartTime = skillParam.BackswingStartTime;
+        skill.sp.AttackAnimationNormalizedStartTime = skillParam.AttackAnimationNormalizedStartTime;
+        skill.sp.AttackAnimationNormalizedEndTime = skillParam.AttackAnimationNormalizedEndTime;
+        skill.sp.ClipName = skillParam.SkillName;
+        skill.sp.Power = skillParam.Power;
+        skill.Action += skill.ClearHitObjectList;
+        //skill.Action += skill.ClearHitPoint;
+        skill.soundAction += skill.PlayEffect;
+        skill.sp.VoicePath = skillParam.Voice;
+        skill.sp.Hps = hitPoints;
+        skill.AddAnimationEvent(hitPoints, skillParam.SkillName);
+        
+        return skill.sp;
+    }
+    
+    /// <summary>
+    /// 攻撃判定を生成する 
+    /// </summary>
+    /// <param name="clipName"></param>
+    /// <param name="startTime"></param>
+    /// <param name="endTime"></param>
+    /// <param name="hitPoint"></param>
+    private void DrawHit(string clipName,float startTime,float endTime,GameObject hitPoint,Animator anim)
+    {
+        AnimationClip[] clips = anim.runtimeAnimatorController.animationClips;
+        
+        foreach (var clip in clips)
+        {
+            if (string.Equals(clip.name, clipName))
+            {
+                AnimationEvent events = new AnimationEvent();
+                events.functionName = "ResetHit";
+                events.time = 0.01f;
+                clip.AddEvent(events);
+            }
+        }
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    private void ResetHit()
+    {
+        m_attacklinePos[0] = m_attacklinePos[1] = Vector3.zero;
+        m_hitObject.Clear();
+    }
+
+
+    /// <summary>
+    /// 動画の再生速度を調整する
+    /// </summary>
+    /// <param name="anim"></param>
+    /// <param name="speed"></param>
+    public static void SetAnimatorSpeed(Animator anim, float speed)
+    {
+        anim.speed = speed;
+    }
+
+    private void AnimPlay()
+    {
+        m_anim.speed = 1;
+    }
+
+    /// <summary>
+    /// ヒット可能な状態に戻る関数を呼び出す
+    /// </summary>
+    private void CallResetCanHit()
+    {
+        //foreach(var i in m_hitObject)
+        //{
+        //    i.SendMessage("SetCanHit");
+        //}
+    }
+
+    public void AddAnimationEvent(GameObject[] hitPoints,string clipName)
+    {
+        foreach (var hp in hitPoints)
+        {
+            DrawHit(clipName, sp.AttackAnimationStartTime, sp.AttackAnimationEndTime, hp, m_anim);
+        }
+
+
+        SetCancel(sp.BackswingStartTime);
+        ResetHit(sp.BackswingStartTime);
+    }
+
+    /// <summary>
+    /// キャンセル可能なタイミングを設定する
+    /// </summary>
+    /// <param name="backswingStartTime"></param>
+    private void SetCancel(float backswingStartTime)
+    {
+        AnimationClip[] clips = m_anim.runtimeAnimatorController.animationClips;
+        foreach (var clip in clips)
+        {
+            if (string.Equals(clip.name, sp.ClipName))
+            {
+                AnimationEvent events = new AnimationEvent();
+                events.functionName = "CallResetCanHit";
+                events.time = backswingStartTime;
+                clip.AddEvent(events);
+            }
+        }
+    }
+
+    private void ResetHit(float backswingStartTime)
+    {
+        AnimationClip[] clips = m_anim.runtimeAnimatorController.animationClips;
+        foreach (var clip in clips)
+        {
+            if (string.Equals(clip.name, sp.ClipName))
+            {
+                AnimationEvent events = new AnimationEvent();
+                events.objectReferenceParameter = m_anim;
+                events.functionName = "CancelState";
+                events.time = backswingStartTime;
+                clip.AddEvent(events);
+            }
+        }
+    }
+
+
+    private void CancelState(Animator anim)
+    {
+        anim.SetBool(cancelID, true);
+    }
+
+
+    private void DisableRootMotion()
+    {
+        m_anim.applyRootMotion = false;
+    }
+
+    private Animator SetAnimationController(GameObject owner)
+    {
+        Animator anim;
+        anim = owner.GetComponent<Animator>();
+        return anim;
+    }
+
+    public void ClearHitObjectList()
+    {
+        m_hitObject.Clear();
+    }
+
+    private void OnAnimationEnd(UnityAction callback)
+    {
+        stateInfo = m_anim.GetCurrentAnimatorStateInfo(0);
+
+        if (stateInfo.normalizedTime > 1.0f)
+        {
+            callback.Invoke();
+        }
+    }
+
+    private void PlayEffect(string path)
+    {
+        AudioManager.Instance.EffectPlay(path, false);
+    }
+
+
+
+    IEnumerator DrawLineFixed(GameObject hitPoint)
+    {
+        int layerMask = (1 << 11) | (1 << 12);
+        m_attacklinePos[1] = hitPoint.transform.position;
+
+        if (m_attacklinePos[0] == Vector3.zero)
+        {
+            m_attacklinePos[0] = m_attacklinePos[1];
+        }
+        if (gameObject.name == "Enemy2001")
+        {
+            Debug.Log(hitPoint.gameObject.ToString());
+        }
+        Debug.DrawLine(m_attacklinePos[0], m_attacklinePos[1], Color.red, 60);
+        RaycastHit hit;
+        if (Physics.Linecast(m_attacklinePos[0], m_attacklinePos[1], out hit, layerMask) || Physics.Linecast(m_attacklinePos[1], m_attacklinePos[0], out hit, layerMask))
+        {
+            Debug.Log("hit");
+            switch (hit.collider.tag)
+            {
+                case "Enemy":
+                    if (!gameObject.CompareTag("Enemy"))
+                    {
+                        if (hit.collider.gameObject.GetComponent<FSM>().canHit)
+                        {
+                            if (!m_hitObject.Exists(ho => ho == hit.collider.gameObject))
+                            {
+                                HitEvent he = new HitEvent(sp.ClipName, sp.Power);
+                                m_hitObject.Add(hit.collider.gameObject);
+                                foreach (var ho in m_hitObject)
+                                {
+                                    soundAction.Invoke(sp.EffectPath);
+                                    ho.GetComponent<FSM>().Damaged(he);
+                                }
+                                Debug.Log(hit.collider.gameObject.name);
+                                Skill.SetAnimatorSpeed(m_anim, 0.3f);
+                                Invoke("AnimPlay", 0.1f);
+
+                            }
+                        }
+                    }
+
+                    break;
+                case "Player":
+                    if(!gameObject.CompareTag("Player"))
+                    {
+                        if (!m_hitObject.Exists(ho => ho == hit.collider.gameObject))
+                        {
+                            HitEvent he = new HitEvent(sp.ClipName, sp.Power);
+                            m_hitObject.Add(hit.collider.gameObject);
+                            foreach (var ho in m_hitObject)
+                            {
+                                ho.GetComponent<PlayerController>().Damaged(he);
+                            }
+                            Debug.Log(hit.collider.gameObject.name);
+                            Skill.SetAnimatorSpeed(m_anim, 0.3f);
+                            Invoke("AnimPlay", 0.1f);
+                        }
+                    }
+
+
+                    break;
+            }
+
+        }
+        m_attacklinePos[0] = m_attacklinePos[1];
+        yield return null;
+    }
+}
